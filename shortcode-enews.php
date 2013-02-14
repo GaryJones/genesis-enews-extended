8c113ace8473398cfb8121244b044d470c8fad73
Brandon Kraft <public@brandonkraft.com>
2013-02-14 01:49:00 -0600

    Initial commit of shortcode php #11

---
+++ shortcode-enews.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Genesis eNews Extended
+ *
+ * @package   BJGK\Genesis_enews_extended
+ * @version   1.1.1
+ * @author    Brandon Kraft <public@brandonkraft.com>
+ * @link      http://www.brandonkraft.com/contrib/plugins/genesis-enews-extended/
+ * @copyright Copyright (c) 2012, Brandon Kraft
+ * @license   GPL-2.0+
+ */
+/**
+ * Shortcode to duplicate functions.
+ *
+ * @package BJGK\Genesis_enews_extended
+ * @author Brandon Kraft <public@brandonkraft.com>
+ * @since  0.0.0
+ */
+function enews_shortcode( $atts ) {
+	extract( shortcode_atts( array(
+		'form-action' => '',
+		'email-name' => 'EMAIL',
+		'email-label' => 'Enter your e-mail address...',
+		'first-name' => '',
+		'first-name-label' => '',
+		'last-name' => '',
+		'last-name-label' => '',
+		'title' => '',
+		'text' => '', //text to show
+		'submit' => '', // submit text
+	), $atts ) );
+	
+	return "the form will be here";
+}
+add_shortcode( 'enews', 'enews_shortcode' );
